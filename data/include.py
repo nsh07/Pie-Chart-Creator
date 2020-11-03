4e943c28c8c92bc04659e786220e26b3f29beb95
ghanteyyy <ghanteyyy@gmail.com>
2020-11-04 00:28:51 +0545

    Major bugs fixed
    Fixed imports
    Fixed images path
    Removed old images
    Added .gitignore file to exclude __pycache__

--- data/include.py
+++ data/include.py
@@ -35,7 +35,7 @@ def initial_position(window, title):
     window.geometry(f'{width}x{height}+{screen_width - width // 2}+{screen_height - height // 2}')
 
     window.resizable(0, 0)
-    window.iconbitmap(resource_path('pics\\icon.ico'))
+    window.iconbitmap(resource_path('..\\pics\\icon.ico'))
     window.title(title)
 
     window.deiconify()
